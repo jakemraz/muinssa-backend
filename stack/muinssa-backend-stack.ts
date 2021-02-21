1e6fda140466100e9808ea6890256366d886f53d
jakemraz <jakemraz100@gmail.com>
2021-02-22 02:03:11 +0900

    add eventbridge for chime status

--- stack/muinssa-backend-stack.ts
+++ stack/muinssa-backend-stack.ts
@@ -4,7 +4,10 @@ import {Runtime} from '@aws-cdk/aws-lambda';
 import {NodejsFunction} from '@aws-cdk/aws-lambda-nodejs';
 import * as ddb from '@aws-cdk/aws-dynamodb';
 import * as iam from '@aws-cdk/aws-iam';
+import * as events from "@aws-cdk/aws-events";
+import * as targets from "@aws-cdk/aws-events-targets";
 import * as path from 'path';
+
 
 export class MuinssaBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -35,7 +38,6 @@ export class MuinssaBackendStack extends cdk.Stack {
       joinFunction.role?.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonChimeFullAccess'));
       table.grantFullAccess(joinFunction);
       
-      
 
       const api = new apigw.RestApi(this, 'MuinssaApi');
       const joinIntegration = new apigw.LambdaIntegration(joinFunction);
@@ -44,6 +46,20 @@ export class MuinssaBackendStack extends cdk.Stack {
       const join = v1.addResource('join');
       const joinPost = join.addMethod('GET', joinIntegration);
 
+
+      const eventBridgeHandler = new NodejsFunction(this, 'EventBridgeFunction', {
+        entry: path.join(__dirname, './function/meeting.js'),
+        runtime: Runtime.NODEJS_14_X,
+        handler: 'event_bridge_handler'
+      });
+
+      const rule = new events.Rule(this, 'rule', {
+        eventPattern: {
+          source: ['aws.chime'],
+          detailType: ['Chime Meeting State Change'],
+        },
+        targets: [new targets.LambdaFunction(eventBridgeHandler)]
+      });
 
   }
 }
